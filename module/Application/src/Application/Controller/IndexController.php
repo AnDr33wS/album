256e28034121d426ba30b181c8d9ba963178f79e
AnDr33wS <williamgerenuttidm@gmail.com>
2016-04-26 11:35:44 -0300

    Teste

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -9,22 +9,80 @@
  */
 namespace Application\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
+use Core\Controller\ActionController;
 use Zend\View\Model\ViewModel;
+use Doctrine\ORM\EntityManager;
+use Application\Model\Album;
+use Application\Form\Album as AlbumForm;
 
-class IndexController extends AbstractActionController {
-	public function indexAction() {
-		return new ViewModel ();
+class IndexController extends ActionController {
+	/**
+	 * @var Doctrine\ORM\EntityManager
+	 */
+	
+	protected $em;
+	public function setEntityManager(EntityManager $em){
+		$this->em = $em;
 	}
-	public function addAction() {
-	}
-	public function editAction() {
-	}
-	public function deleteAction() {
-    	
-    }
+	 public function getEntityManager(){
+	 	if (null === $this->em){
+	 		$this->em = $this->getServiceLocator ()->get ('Doctrine\ORM\EntityManager');
+	 	}
+	 	return $this->em;
+	 }
+	 public function indexAction(){
+	 	//albuns pode ser album, testar posteriormente caso ocorram erros.
+	 	$albuns = $this->getEntityManager()->getRepository ('Application\Model\Album')->findAll();
+	 	return new ViewMOdel (array(
+	 			'albuns'=>$albuns
+	 	));
+	 }
+	 public function saveAction(){
+	 	$form = new AlbumForm();
+	 	$request = $this->getRequest();
+	 	if ($request-> isAlbum()){
+	 		$album = new Album();
+	 		$form->setInputFilter ($album->getInpitFilter ());
+	 		$form_setData ($request->getAlbum());
+	 		if($form->isValid()){
+	 			$data = $form->getData();
+	 			if (isset ($data ['id']) && $data>0){
+	 				$album = $this->getEntityManager ()->find ('Application\Model\Album',$data['id']);
+	 			}
+	 			unset($data['submit']);
+	 			$Album->setData($data);
+	 			$this->getEntityManager()->persist($album);
+	 			$this->getEntityManager ()->flush ();
+	 			return $this->redirect ()->toUrl ( '/application' );
+	 			}
+	 		}
+	 		else {
+	 			$id = ( int ) $this->params ()->fromRoute ( 'id', 0 );
+	 			if ($id > 0) {
+	 				$album = $this->getEntityManager ()->find ( 'Application\Model\Album', $id );
+	 				$form->bind ( $album );
+	 				$form->get ( 'submit' )->setAttribute ( 'value', 'Edit' );
+	 			}
+	 		}
+	 		}
+	 		public function deleteAction() {
+	 			$id = ( int ) $this->params ()->fromRoute ( 'id', 0 );
+	 			if ($id == 0) {
+	 				throw new \exception ( "Código obrigatório" );
+	 			}
+	 			$album = $this->getEntityManager ()->find ( 'Application\Model\Album', $id );
+	 			if ($album) {
+	 				$this->getEntityManager ()->remove ( $album );
+	 				$this->getEntityManager ()->flush ();
+	 			}
+	 			return $this->redirect ()->toUrl ( '/application' );
+	 		
+	 		
+	 		}
+	 		}
+	 	
+	 
+   
     
     
-    
-    
-}
+
--- module/Application/src/Application/Model/Album.php
+++ module/Application/src/Application/Model/Album.php
@@ -13,10 +13,9 @@ use Doctrine\ORM\Mapping as ORM;
  * @category Application
  * @package Model
  *          @ORM\Entity
- *          @ORM\Table(name="albuns") 
+ *          @ORM\Table(name="albuns")
  */
-//@ORM\Table(name="albuns") = database do banco
-
+// @ORM\Table(name="albuns") = database do banco
 class Album extends Entity {
 	
 	/**
@@ -28,17 +27,103 @@ class Album extends Entity {
 	/**
 	 * @ORM\column(type="string")
 	 */
-	
 	protected $artist;
 	
 	/**
 	 * @ORM\column(type="string")
 	 */
-	
 	protected $title;
-	
-	
+	public function getId() {
+		return $this->$id;
+	}
+	public function setId($id) {
+		$this->id = $id;
+	}
+	public function getArtist() {
+		return $this->$artist;
+	}
+	public function setArtist($artist) {
+		$this->artist = $artist;
+	}
+	public function getTitle() {
+		return $this->$title;
+	}
+	public function setTitle($title) {
+		$this->artist = $artist;
+	}
+	/**
+	 * Configura os filtros dos campos da entidade
+	 *
+	 * @return Zend\InputFilter\InputFilter
+	 */
+	public function getInputFilter(){
+		if(! $this->inputFilter){
+			$inputFilter = new InputFilter();
+			$factory = new InputFactory();
+			$inputFilter->add ( $factory->createInput (array(
+					//filtros para ID no banco
+					'name' => 'id',
+					'required' => true,
+					'filters' => array(
+							array(
+									'name'=>'Int'
+							)
+					)
+			)));
+			$inputFilter->add ($factory->createInput (array(
+					'name'=> 'artist',
+					'required'=> true,
+					'filters' => array(
+							array(
+									'name'=>'StringTags'
+									),
+							array(
+									'name'=>'StringTrim'
+							)
+					),
+			
+					'validators' => array(
+							array(
+									'name'=>'StringLength',
+									'options'=> array(
+											'encoding'=>'UTF-8',
+											'min' => 1,
+											'max' => 100
+									)
+							)
+					)
+			)));
+			$inputFilter->add ($factory->createInput (array(
+					'name'=> 'tile',
+					'required'=> true,
+					'filters' => array(
+							array(
+									'name'=>'StringTags'
+							),
+							array(
+									'name'=>'StringTrim'
+							)
+					),
+						
+					'validators' => array(
+							array(
+									'name'=>'StringLength',
+									'options'=> array(
+											'encoding'=>'UTF-8',
+											'min' => 1,
+											'max' => 100
+									)
+							)
+					)
+			)));		
+		}
+		return $this->inputFilter;
+	}
 	
 	
 	
 }
+
+	
+	
+	
